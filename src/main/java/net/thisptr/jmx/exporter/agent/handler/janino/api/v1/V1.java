3cc5237a6ee6025b95ff32089d5ae3d0cf941aaa
Eiichi Sato <sato.eiichi@gmail.com>
2020-08-07 04:01:06 +0900

    engine(janino): improve V1.snakeCase() so that SSL formats to ssl

--- src/main/java/net/thisptr/jmx/exporter/agent/handler/janino/api/v1/V1.java
+++ src/main/java/net/thisptr/jmx/exporter/agent/handler/janino/api/v1/V1.java
@@ -31,34 +31,70 @@ public class V1 {
 			return Math.max(1, name.length() * 2);
 		}
 
+		private static class CharactorGroup {
+			private static final byte encode(final boolean subsequentLeadingCapitalNeedsUnderscore, final boolean subsequentCapitalNeedsUnderscore) {
+				byte val = 0;
+				if (subsequentLeadingCapitalNeedsUnderscore)
+					val |= 0b10;
+				if (subsequentCapitalNeedsUnderscore)
+					val |= 0b01;
+				return val;
+			}
+
+			private static final byte LOWER_CASE_ALPHABET = encode(true, true);
+			private static final byte UPPER_CASE_ALPHABET = encode(true, false);
+			private static final byte NUMBER = encode(true, true);
+			private static final byte COLON_OR_UNDERSCORE = encode(false, false);
+			private static final byte OTHER = encode(false, false);
+			private static final byte INITIAL = encode(false, false);
+
+			// Leading capital is a upper-case alphabet that is immediately followed by a lower-case letter, for example,
+			// * F in "Foo"
+			// * C in "SSLCertificate"
+			public static final boolean subsequentLeadingCapitalNeedsUnderscore(final byte type) {
+				return (type & 0b10) > 0;
+			}
+
+			public static final boolean subsequentCapitalNeedsUnderscore(final byte type) {
+				return (type & 0b01) > 0;
+			}
+		}
+
 		@Override
 		public int write(final String name, final byte[] bytes, int index) {
 			final int savedIndex = index;
 			int length = name.length();
-			boolean suppressUnderscore = true;
+			byte pcharType = CharactorGroup.INITIAL;
 			for (int i = 0; i < length; ++i) {
 				final char ch = name.charAt(i);
 				if ('a' <= ch && ch <= 'z') {
 					bytes[index++] = (byte) ch;
-					suppressUnderscore = false;
+					pcharType = CharactorGroup.LOWER_CASE_ALPHABET;
 				} else if ('A' <= ch && ch <= 'Z') {
-					if (!suppressUnderscore)
-						bytes[index++] = '_';
+					final char nchar = i + 1 < length ? name.charAt(i + 1) : 0;
+					if ('a' <= nchar && nchar <= 'z') {
+						if (CharactorGroup.subsequentLeadingCapitalNeedsUnderscore(pcharType))
+							bytes[index++] = '_';
+					} else { // nchar = [^a-z]
+						if (CharactorGroup.subsequentCapitalNeedsUnderscore(pcharType))
+							bytes[index++] = '_';
+					}
 					bytes[index++] = (byte) (ch + ('a' - 'A'));
-					suppressUnderscore = false;
+					pcharType = CharactorGroup.UPPER_CASE_ALPHABET;
 				} else if (ch == ':' || ch == '_') {
 					bytes[index++] = (byte) ch;
-					suppressUnderscore = true;
+					pcharType = CharactorGroup.COLON_OR_UNDERSCORE;
 				} else if ('0' <= ch && ch <= '9') {
-					if (savedIndex == index)
+					if (savedIndex == index) {
 						bytes[index++] = '_'; // first char cannot be a number; prepend _;
+					}
 					bytes[index++] = (byte) ch;
-					suppressUnderscore = false;
+					pcharType = CharactorGroup.NUMBER;
 				} else {
 					if (Character.isHighSurrogate(ch))
 						++i;
 					bytes[index++] = '_';
-					suppressUnderscore = true;
+					pcharType = CharactorGroup.OTHER;
 				}
 			}
 			if (savedIndex == index) { // empty metric name is not allowed
--- src/test/java/net/thisptr/jmx/exporter/agent/handler/janino/api/v1/V1Test.java
+++ src/test/java/net/thisptr/jmx/exporter/agent/handler/janino/api/v1/V1Test.java
@@ -101,6 +101,38 @@ public class V1Test {
 		assertThat(snakeCase("::test_leading_dup_colon")).isEqualTo("::test_leading_dup_colon");
 		assertThat(snakeCase("test::test_dup_colon")).isEqualTo("test::test_dup_colon");
 		assertThat(snakeCase("test::Test_dup_colon_and_capital")).isEqualTo("test::test_dup_colon_and_capital");
+
+		assertThat(snakeCase("Catalina_SSLHostConfig_certificateVerificationDepth")).isEqualTo("catalina_ssl_host_config_certificate_verification_depth");
+		assertThat(snakeCase("Catalina:SSLHostConfig:certificateVerificationDepth")).isEqualTo("catalina:ssl_host_config:certificate_verification_depth");
+
+		// This case-style is just weird. I don't think we can support this without compromising something.
+		assertThat(snakeCase("Catalina_ProtocolHandler_sSLVerifyDepth")).isEqualTo("catalina_protocol_handler_s_sl_verify_depth");
+		assertThat(snakeCase("Catalina:ProtocolHandler:sSLVerifyDepth")).isEqualTo("catalina:protocol_handler:s_sl_verify_depth");
+
+		assertThat(snakeCase("Catalina_SSLH{stConfig_certificateVerificationDepth")).isEqualTo("catalina_sslh_st_config_certificate_verification_depth");
+		assertThat(snakeCase("Catalina_SSLH_stConfig_certificateVerificationDepth")).isEqualTo("catalina_sslh_st_config_certificate_verification_depth");
+
+		assertThat(snakeCase("Catalina_SSL")).isEqualTo("catalina_ssl");
+		assertThat(snakeCase("Catalina_S")).isEqualTo("catalina_s");
+
+		assertThat(snakeCase("Catalina:ProtocolHandler:Foo_SSLVerifyDepth")).isEqualTo("catalina:protocol_handler:foo_ssl_verify_depth");
+		assertThat(snakeCase("Catalina:ProtocolHandler:FooSSLVerifyDepth")).isEqualTo("catalina:protocol_handler:foo_ssl_verify_depth");
+		assertThat(snakeCase("Catalina:ProtocolHandler:Foo:SSLVerifyDepth")).isEqualTo("catalina:protocol_handler:foo:ssl_verify_depth");
+		assertThat(snakeCase("Catalina:ProtocolHandler:Foo{SSLVerifyDepth")).isEqualTo("catalina:protocol_handler:foo_ssl_verify_depth");
+
+		assertThat(snakeCase("SSL")).isEqualTo("ssl");
+		assertThat(snakeCase("SSL{test")).isEqualTo("ssl_test");
+		assertThat(snakeCase("SSL_test")).isEqualTo("ssl_test");
+		assertThat(snakeCase("SSLtest")).isEqualTo("ss_ltest");
+		assertThat(snakeCase("SSLTest")).isEqualTo("ssl_test");
+
+		assertThat(snakeCase("STest")).isEqualTo("s_test");
+		assertThat(snakeCase("TestS")).isEqualTo("test_s");
+		assertThat(snakeCase("{STest")).isEqualTo("_s_test");
+		assertThat(snakeCase("0STest")).isEqualTo("_0_s_test");
+		assertThat(snakeCase("0sTest")).isEqualTo("_0s_test");
+		assertThat(snakeCase("aSTest")).isEqualTo("a_s_test");
+		assertThat(snakeCase("aaSTest")).isEqualTo("aa_s_test");
 	}
 
 	@Test
