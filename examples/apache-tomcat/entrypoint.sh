2ca2859dd4211e163ac3370955e7a68c8ff3b7ce
Eiichi Sato <sato.eiichi@gmail.com>
2020-08-08 20:03:04 +0900

    example(apache-tomcat): remove unused file

--- examples/apache-tomcat/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-exec java ${JAVA_OPTS:-} -cp $FLUME_HOME/conf:$FLUME_HOME/lib/*:/lib/* -Djava.library.path= -Dflume.root.logger=INFO,console org.apache.flume.node.Application -f $FLUME_HOME/conf/flume-conf.properties -n agent --no-reload-conf
