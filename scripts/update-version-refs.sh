68070cbc6baaf4ca6a54d1b0c756a3b8a3abc2fd
Eiichi Sato <sato.eiichi@gmail.com>
2020-08-10 09:26:51 +0900

    update scripts/update-version-refs.sh

--- scripts/update-version-refs.sh
+++ scripts/update-version-refs.sh
@@ -14,3 +14,5 @@ version="$1"
 
 sed -i "s;https://repo1.maven.org/maven2/net/thisptr/java-prometheus-metrics-agent/[0-9a-z.-]*/java-prometheus-metrics-agent-[0-9a-z.-]*.jar;https://repo1.maven.org/maven2/net/thisptr/scriptable-jmx-exporter/$version/scriptable-jmx-exporter-$version.jar;g" README.md
 sed -i "s;https://repo1.maven.org/maven2/net/thisptr/scriptable-jmx-exporter/[0-9a-z.-]*/scriptable-jmx-exporter-[0-9a-z.-]*.jar;https://repo1.maven.org/maven2/net/thisptr/scriptable-jmx-exporter/$version/scriptable-jmx-exporter-$version.jar;g" README.md
+sed -i "s;https://raw.githubusercontent.com/eiiches/scriptable-jmx-exporter/v[0-9a-z.-]*/;https://raw.githubusercontent.com/eiiches/scriptable-jmx-exporter/v$version/;g" README.md
+sed -i "s;scriptable-jmx-exporter-[0-9a-z.-]*.jar;scriptable-jmx-exporter-$version.jar;g" README.md
